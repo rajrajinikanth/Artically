5b8f86b27319f7bbc407de5be028618b6a85c3e5
rajrajinikanth <96593526+rajrajinikanth@users.noreply.github.com>
2023-07-18 20:50:41 -0500

    Removed redis reference and configuration

--- app.js
+++ app.js
@@ -1,118 +1,113 @@
 'use strict';
- 
+
 import got from 'got';
 import * as chrono from 'chrono-node';
+import { createRequire } from 'module';
+import { JSDOM } from 'jsdom';
+import urlHandler from 'url';
+import urljoin from 'url-join';
+import cheerio from 'cheerio';
+import WordPressApi from 'wpapi';
+import dotenv from 'dotenv';
 
-// Define "require"
-import { createRequire } from "module";
 const require = createRequire(import.meta.url);
+dotenv.config();
 
-var { Readability } = require('@mozilla/readability');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const urlHandler = require('url');
-var urljoin = require('url-join');
-const cheerio = require('cheerio');
-var WordPressApi = require( 'wpapi' );
-const Redis = require("ioredis");
-
-const REDIS_URL = "192.168.86.250";
-const REDIS_PORT = 6379;
-const redis = new Redis(REDIS_PORT, REDIS_URL);  
 const cliParams = process.argv.slice(2);
-var docName = cliParams[0];
-var configKey = 'artically.'+ docName +'.config'
+const docName = cliParams[0];
+const configKey = `artically.${docName}.config`;
 
-var config = JSON.parse(await getRedisValue(configKey));
- 
-var content = await getArticle(config);
-await publishArticle(content);
+async function main() {
+  const config = JSON.parse(getEnvValue(configKey));
 
-async function getArticle(configitem){
-
-    var editorial_home_url = configitem.editorial_home_url;
-    var pattern = configitem.editorial_article_pattern;
-    const contentBody = await got(editorial_home_url);
-    const $content = cheerio.load(contentBody.body);
-    var articleUrl = resolveUrl(editorial_home_url,$content(pattern).attr('href'));
-    
-    const {body} = await got(articleUrl); 
-    const $ = cheerio.load(body);
-    const dom = new JSDOM(body);
-    let reader = new Readability(dom.window.document);
-    let article = reader.parse();
-    var article_content = article.textContent.replace(/\r?\n|\r/g, " ");
-    var article_html = article.content.replace(/\r?\n|\r/g, " ");
-    var jsonArticle = {
-        articleUrl: articleUrl,
-        articleDt: getArticleDate($, configitem.editorial_date_pattern),
-        articleTitle: article.title,
-        articleContent: article_content.trim(),
-        articleAuthor: configitem.editorial_author
-    };
-
-    return jsonArticle;
+  const content = await getArticle(config);
+  await publishArticle(content);
 }
 
-async function publishArticle(content){
-    var wp_endpoint = await getRedisValue('artically.wordpress.endpoint');
-    var wp_credentials = decodeBase64(await getRedisValue('artically.wordpress.credentials'));
-    var wp_username = wp_credentials.split(':')[0];
-    var wp_password = wp_credentials.split(':')[1];
- 
-       var wp = new WordPressApi({
-           endpoint: wp_endpoint,
-           username: wp_username,
-           password: wp_password
-       });
+async function getArticle(configitem) {
+  const { editorial_home_url, editorial_article_pattern, editorial_date_pattern, editorial_author } = configitem;
+  const contentBody = await got(editorial_home_url);
+  const $content = cheerio.load(contentBody.body);
+  const articleUrl = resolveUrl(editorial_home_url, $content(editorial_article_pattern).attr('href'));
 
-      var tag =  await wp.tags().slug(docName).then(function( tag ) { return(tag[0].id); });
-      var cat =  await wp.categories().slug('editorial').then(function( cat ) { return(cat[0].id); });
+  const { body } = await got(articleUrl);
+  const $ = cheerio.load(body);
+  const dom = new JSDOM(body);
+  const reader = new Readability(dom.window.document);
+  const article = reader.parse();
+  const article_content = article.textContent.replace(/\r?\n|\r/g, ' ');
+  const article_html = article.content.replace(/\r?\n|\r/g, ' ');
+  const jsonArticle = {
+    articleUrl: articleUrl,
+    articleDt: getArticleDate($, editorial_date_pattern),
+    articleTitle: article.title,
+    articleContent: article_content.trim(),
+    articleAuthor: editorial_author,
+  };
 
-       var metaformat = await getRedisValue('artically.metaformat.config');
+  return jsonArticle;
+}
 
-       metaformat = metaformat.replace('@article.sourceurl',content.articleUrl);
-       metaformat = metaformat.replace('@article.sourcedate',content.articleDt);
-       metaformat = metaformat.replace('@article.sourcename',docName);
+async function publishArticle(content) {
+  const wp_endpoint = process.env.WORDPRESS_ENDPOINT;
+  const wp_credentials = decodeBase64(process.env.WORDPRESS_CREDENTIALS);
+  const wp_username = wp_credentials.split(':')[0];
+  const wp_password = wp_credentials.split(':')[1];
 
-       wp.posts().create({
-           title: content.articleTitle,
-           content: metaformat + content.articleContent,
-           slug:docName,
-           tags: [tag],
-           categories: [cat],
-           status: 'publish',
-       }).then(function( response ) {
-           console.log( response.id );
-       }).catch(function( err ) {
-           console.log(err);
-       });
-   }
+  const wp = new WordPressApi({
+    endpoint: wp_endpoint,
+    username: wp_username,
+    password: wp_password,
+  });
 
-async function getRedisValue(Key){
-    const config = redis.get(Key).then(function (result) {        
-		return result;
-      });
-    return config;
-} 
+  const tag = await wp.tags().slug(docName).then((tag) => tag[0].id);
+  const cat = await wp.categories().slug('editorial').then((cat) => cat[0].id);
+
+  let metaformat = getEnvValue('artically.metaformat.config');
+
+  metaformat = metaformat.replace('@article.sourceurl', content.articleUrl);
+  metaformat = metaformat.replace('@article.sourcedate', content.articleDt);
+  metaformat = metaformat.replace('@article.sourcename', docName);
+
+  wp.posts()
+    .create({
+      title: content.articleTitle,
+      content: metaformat + content.articleContent,
+      slug: docName,
+      tags: [tag],
+      categories: [cat],
+      status: 'publish',
+    })
+    .then((response) => {
+      console.log(response.id);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function getEnvValue(key) {
+  const value = process.env[key];
+  return value;
+}
 
 function getArticleDate($, expr) {
-    var value = $(expr).first().text() || $(expr).text() || $(expr).attr('content');
-    return typeof value !== 'undefined' && value ? chrono.parseDate(value) : '';
+  const value = $(expr).first().text() || $(expr).text() || $(expr).attr('content');
+  return typeof value !== 'undefined' && value ? chrono.parseDate(value) : '';
 }
 
-function resolveUrl(baseUrl,articleUrl){				
-	var parsedBaseUrl = urlHandler.parse(baseUrl, true,true)
-	var parsedArticleUrl = urlHandler.parse(articleUrl, true,true)
-	var protocol = parsedArticleUrl.protocol ?? parsedBaseUrl.protocol;
-	var urlPath = parsedArticleUrl.path.indexOf("/") != -1 ? parsedArticleUrl.path : '/' + parsedArticleUrl.path;
-	var resolvedUrl =  protocol + "//" + parsedBaseUrl.host + urlPath;
-	return resolvedUrl;		
+function resolveUrl(baseUrl, articleUrl) {
+  const parsedBaseUrl = urlHandler.parse(baseUrl, true, true);
+  const parsedArticleUrl = urlHandler.parse(articleUrl, true, true);
+  const protocol = parsedArticleUrl.protocol ?? parsedBaseUrl.protocol;
+  const urlPath = parsedArticleUrl.path.indexOf('/') != -1 ? parsedArticleUrl.path : '/' + parsedArticleUrl.path;
+  const resolvedUrl = protocol + '//' + parsedBaseUrl.host + urlPath;
+  return resolvedUrl;
 }
 
-function decodeBase64(enc_value){
-	let buff = Buffer.from(enc_value, 'base64');  
-	return buff.toString('utf-8');
+function decodeBase64(enc_value) {
+  const buff = Buffer.from(enc_value, 'base64');
+  return buff.toString('utf-8');
 }
 
-redis.disconnect();
+main();
